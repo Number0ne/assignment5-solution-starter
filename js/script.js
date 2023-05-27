a5f7ebdb2d9438fba8a09da6e54e682c15382362
Ndifreke Okpo <65951359+Number0ne@users.noreply.github.com>
2023-05-27 21:10:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -103,13 +103,19 @@ $(function () {
           // return category object with that randomArrayIndex
           return categories[randomArrayIndex];
         }
-        // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-        // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-        // variable's name implies it expects.
+
+        switchMenuToActive();
 
         var chosenCategoryIndex = chooseRandomCategory(categories);
         console.log(categories[chosenCategoryIndex]);
         var chosenCategoryShortName = categories[chosenCategoryIndex];
+
+        var homeHtmlToInsertIntoMainPage = buildCategoriesViewHtml(
+          chosenCategoryShortName,
+          categoriesTitleHtml,
+          homeHtml
+        );
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
         // chosen category from STEP 2. Use existing insertProperty function for that purpose.
